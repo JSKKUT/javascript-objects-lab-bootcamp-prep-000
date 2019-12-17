d4d9eec1eb06dce156afd9b6d24d9adba4554e63
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-17 00:01:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
     return object
 }
+
+
+Objects updateObjectWithKeyAndValue(object, key, value) does not modify the original object, but rather returns a clone with the new data
