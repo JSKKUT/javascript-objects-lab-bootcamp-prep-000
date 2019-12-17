c018bd1bec9483f4637eda58ed316550e80d7560
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-17 01:12:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value})
+  return Object.assign({}, object, {[key]: value});
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
-    return object
+    return object;
 }
 
 function deleteFromObjectByKey(object, key) {
@@ -12,6 +12,11 @@ function deleteFromObjectByKey(object, key) {
     delete object.key;
   };
       return newObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+    return object;
 }
 
 
