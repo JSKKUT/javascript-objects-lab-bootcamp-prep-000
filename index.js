3af275039dc085ffbcc409896c77ffe903e13838
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-17 01:03:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function deleteFromObjectByKey(object, key) {
   };
       return newObject;
 }
+
+
+Objects deleteFromObjectByKey(object, key) deletes `key` from a clone of object and returns the new object (it is non-destructive)
+Objects deleteFromObjectByKey(object, key) does not modify the original object (it is non-destructive)
