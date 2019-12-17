141bebb81f8b5d1ba9c3383477c98ac8681c2461
global-domain-9186 <jasonscottkutner@gmail.com>
2019-12-17 00:11:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,17 +5,3 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 
 
-Objects updateObjectWithKeyAndValue(object, key, value) does not modify the original object, but rather returns a clone 
-with the new data:
-
-
-
-
-
-function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
-    return object
-}
-Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs 
-and the new key value pair:
-Objects updateObjectWithKeyAndValue(object, key, value) returns an object with an updated key value pair:
